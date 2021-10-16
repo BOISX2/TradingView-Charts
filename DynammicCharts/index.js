d0b29127f4a15fe81ee84fdd61fd9c5b2ef4191d
Dhanush <gdhanush@am.students.amrita.edu>
2021-10-16 15:50:21 +0530

    added dynamic charts

---
+++ DynammicCharts/index.js
@@ -0,0 +1,33 @@
+const log = console.log;
+
+const chartProperties = {
+  width:1500,
+  height:600,
+  timeScale:{
+    timeVisible:true,
+    secondsVisible:false,
+  }
+}
+
+const domElement = document.getElementById('tvchart');
+const chart = LightweightCharts.createChart(domElement,chartProperties);
+
+
+const candleSeries = chart.addCandlestickSeries();
+
+// fetch(`http://127.0.0.1:9665/fetchAPI?endpoint=https://api.binance.com/api/v3/klines?symbol=ETHUSDT&interval=1d&limit=1000`)
+//   .then(res => res.json())
+//   .then(data => {
+//     const cdata = data.map(d => {
+//       return {time:d[0]/1000,open:parseFloat(d[1]),high:parseFloat(d[2]),low:parseFloat(d[3]),close:parseFloat(d[4])}
+//     });
+//     candleSeries.setData(cdata);
+//   })
+//   .catch(err => log(err))
+
+const socket = io.connect('http://127.0.0.1:4000/');
+
+socket.on('KLINE',(pl)=>{
+  //log(pl);
+  candleSeries.update(pl);
+});
